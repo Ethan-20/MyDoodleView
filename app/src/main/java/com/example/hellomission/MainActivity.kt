cdb313db715b575e12aa31cef7260f447de08d05
yqg <i_yaoqiaoguang@cvte.com>
2023-10-16 17:15:43 +0800

    创建并切换到 test 分支，在 test 分支中修改打印内容 Working in cvte

--- app/src/main/java/com/example/hellomission/MainActivity.kt
+++ app/src/main/java/com/example/hellomission/MainActivity.kt
@@ -15,32 +15,6 @@ import com.example.hellomission.ui.theme.HellomissionTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            HellomissionTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    HellomissionTheme {
-        Greeting("Android")
+        println("Working in cvte")
     }
 }
